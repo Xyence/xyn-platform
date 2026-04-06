83915e40a809575cce2db90724a2fc82ff5307db
Joshua Restivo <jrestivo@xyence.io>
2026-04-06 16:22:18 -0500

    migrations: merge xyn_orchestrator dual heads at 0154/0162

---
+++ services/xyn-api/backend/xyn_orchestrator/migrations/0163_merge_20260406_0001.py
@@ -0,0 +1,11 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("xyn_orchestrator", "0154_artifact_owner_repo_slug_default"),
+        ("xyn_orchestrator", "0162_releasetargetexecutionstepevidence"),
+    ]
+
+    operations = []
